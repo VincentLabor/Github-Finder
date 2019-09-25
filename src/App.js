9bbd4bffb8ea1788e0a212eb2780b416e610b175
VincentLabor <vlabor95@gmail.com>
2019-09-24 23:55:13 -0700

    Search function is operational

--- src/App.js
+++ src/App.js
@@ -10,20 +10,35 @@ class App extends Component {
     users: [],
     loading: false
   }
-  async componentDidMount() {
-    this.setState({ loading: true });
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
 
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({ users: res.data, loading: false });
+  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  //   this.setState({ users: res.data, loading: false });
+  // } This layouts about 100 users. 
+
+  //Search Github users
+  searchUsers = async text => {
+    this.setState({ loading: true })
+    //console.log(text); Now we will make a get query since we know that the text and search work pretty well.
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ users: res.data.items, loading: false }); //This is more specific and will leave out things like pagination
+  }
+
+  clearUsers = () => {
+    this.setState({ users: [], loading: false })
   }
 
   render() {
+
+    const {users, loading} = this.state;
+
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
-          <Search />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,21 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import PropTypes from 'prop-types'
+
 
 class Search extends Component {
     state = {
         text: ''
     }
 
-    onChange = (e) => {
-        this.setState({text:e.target.value})
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired
     }
 
-    render(){
-        return(
+    onChange = (e) => this.setState({ [e.target.name]: e.target.value })
+
+    onSubmit = (e) => { //We are attempting to send a prop back upwards towards app
+        e.preventDefault(); //This prevents a link from being submitted.
+        
+        this.props.searchUsers(this.state.text); //we can call this where we embedded search on App.js
+        this.setState({ text: '' })
+    }
+
+    render() {
+        const {showClear, clearUsers} = this.props;
+        const {text} = this.state
+        return (
             <div>
-                <form className="form">
-                    <input type = "text" name="text" placeholder="Search Users" value = {this.state.text} onChange = {this.onChange}/>
-                    <input type = "submit" value = "Search" className = "btn-dark btn-block"/>
+                <form onSubmit={this.onSubmit} className="form">
+                    <input
+                        type="text"
+                        name="text"
+                        placeholder="Search Users"
+                        value={text}
+                        onChange={this.onChange} /* This is required if I want the entered text in the search bar to be part of the state.  */
+                    />
+                    <input type="submit" value="Search" className="btn-dark btn-block" />
                 </form>
+                {showClear && <div className="btn btn-light btn-block" onClick={clearUsers}>Clear</div>}
+
             </div>
         )
     }
