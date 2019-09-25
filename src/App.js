8b8f6bf691d4fefa673ee9ea0b6b85db12c18971
VincentLabor <vlabor95@gmail.com>
2019-09-24 22:20:27 -0700

    Search form added

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
+import Search from './components/users/Search';
 
 class App extends Component {
   state = {
     users: [],
     loading: false
   }
-async componentDidMount() {
-this.setState({loading:true});
+  async componentDidMount() {
+    this.setState({ loading: true });
 
-  const res = await axios.get('https://api.github.com/users');
-  this.setState({users: res.data, loading:false});
-}
-  
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className="App">
-        <Navbar title = "Github Finder" icon = "fab fa-github"/>
+        <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
-          <Users loading = {this.state.loading} users = {this.state.users}/>
-          </div>
+          <Search />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
